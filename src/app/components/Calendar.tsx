98f1c5c90b2d7330be74b2ab0356766b2e0d6392
Yeaeun Han <yenhan.dev@gmail.com>
2023-02-24 16:32:54 -0500

    feat: add calendar date picker

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+
+export default function Calendar() {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs(Date.now()));
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+        openTo="day"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Calendar from "@/app/components/Calendar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Calendar />
+    </main>
+  );
 }
