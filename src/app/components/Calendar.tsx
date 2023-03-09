d4bd8ec3e76afd8fa11baeb58df78c94570778ce
Yeaeun Han <yenhan.dev@gmail.com>
2023-03-08 22:41:52 -0500

    add tags section on edit

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -3,29 +3,16 @@ import dayjs, { Dayjs } from "dayjs";
 import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
-
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 export default function Calendar() {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs(Date.now()));
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DesktopDatePicker
-        defaultValue={dayjs("2022-04-17")}
+      <DatePicker
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
         sx={{ bgcolor: "white", borderRadius: "4px", width: "100%" }}
       />
-      {/* <DatePicker
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            sx={{ bgcolor: "white", borderRadius: "4px", width: "100%" }}
-          />
-        )}
-      /> */}
     </LocalizationProvider>
   );
 }
--- src/app/components/EditTimeLine/EditTimeLine.tsx
+++ src/app/components/EditTimeLine/EditTimeLine.tsx
@@ -7,48 +7,92 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 export default function StatisticsByTag() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box
       className="form"
       sx={{
-        width: "60%",
+        width: "100%",
         padding: "2rem",
-        border: "1px black solid",
+        border: "1px #1a1a1a solid",
         borderRadius: "4px",
         backgroundColor: "white",
       }}
     >
       <Typography variant="h5" component="h5" sx={{ mb: "1.5rem" }}>
-        Add New Time Line
+        New Time Line
       </Typography>
       <TextField
         id="outlined-basic"
         label="Name"
         variant="outlined"
-        sx={{ width: "100%", mb: "1.5rem" }}
+        sx={{ width: "100%", mb: "1rem" }}
       />
-      <LocalizationProvider
-        dateAdapter={AdapterDayjs}
-        sx={{ width: "100%", justifyContent: "space-between" }}
+      <Container
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          mb: "1rem",
+          pl: { xs: "0", md: "0" },
+          pr: { xs: "0", md: "0" },
+        }}
       >
-        <MobileTimePicker
-          label="From"
-          defaultValue={dayjs("2022-04-17T15:30")}
-          sx={{ width: "45%", mb: "1.5rem", mr: "7%" }}
-        />
-        <MobileTimePicker
-          label="To"
-          defaultValue={dayjs("2022-04-17T15:30")}
-          sx={{ width: "45%", mb: "1.5rem" }}
-        />
-      </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <MobileTimePicker
+            label="From"
+            defaultValue={dayjs("2022-04-17T15:30")}
+            sx={{ width: "48%" }}
+          />
+          <MobileTimePicker
+            label="To"
+            defaultValue={dayjs("2022-04-17T15:30")}
+            sx={{ width: "48%" }}
+          />
+        </LocalizationProvider>
+      </Container>
       <TextField
         id="outlined-basic"
         label="Description"
         variant="outlined"
-        sx={{ width: "100%", mb: "1.5rem" }}
+        sx={{ width: "100%", mb: "1rem" }}
       />
+      <Box sx={{ width: "100%", mb: "1rem" }}>
+        <Typography variant="subtitle2" gutterBottom>
+          Tags
+        </Typography>
+        <IconButton aria-label="add" onClick={handleClickOpen}>
+          <AddCircleOutlineOutlinedIcon />
+        </IconButton>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Add Tags</DialogTitle>
+          <DialogContent>
+            <Button disabled>Add tags</Button>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Add</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
       <Button variant="contained">Add</Button>
     </Box>
   );
